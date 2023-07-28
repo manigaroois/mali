17c3d94da0fb79b607df3e83df7d794ccdd8f163
manigaroosi <manigaroosicoder@gmail.com>
2023-07-29 00:36:02 +0330

    Im added class(Server,AbrServer,Income,Expense) ,and added this callass on admin.py

--- web/models.py
+++ web/models.py
@@ -1,5 +1,55 @@
+import datetime
+
+from datetime import timezone
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
+from django.utils.translation import gettext_lazy as _
+
+class Server(models.Model):
+    title = models.CharField(_("عنوان"), max_length=255)
+
+    def __str__(self):
+        return self.title
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class AbrServer(models.Model):
+    title = models.CharField(_("عنوان"), max_length=255)
+
+    def __str__(self):
+        return self.title
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Expense(models.Model):
-    titel = models.CharField()
+    title = models.CharField(_("عنوان"), max_length=255)
+    product = models.TextField(_("محصول"))
+    user_name = models.CharField(_("نام کاربری خریدار"), max_length=255)
+    amount = models.BigIntegerField(_("قیمت"))
+    time = models.DateTimeField(_("زمان"))
+    server = models.ForeignKey(AbrServer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Income(models.Model):
+    title = models.CharField(_("عنوان"), max_length=255)
+    product = models.TextField(_("محصول"))
+    user_name = models.CharField(_("نام کاربری خریدار"), max_length=255)
+    amount = models.BigIntegerField(_("قیمت"))
+    time = models.DateTimeField(_("زمان"))
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
