171ba7fe6c646d98a2d87fd2caa5205766ebc2bb
manigaroosi <manigaroosicoder@gmail.com>
2023-08-02 21:31:48 +0330

    add simple template

--- web/views.py
+++ web/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Income,Expense
 
-# Create your views here.
+def main(request):
+    return render(request, 'main.html')
+
+def management(request):
+    income = Income.objects.order_by('-time')
+    return render(request, 'table.html',{'income':income})
+
